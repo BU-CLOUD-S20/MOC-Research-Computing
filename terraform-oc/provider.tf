25eace88895fd2393c8f2e279b387270936bb694
NicoPache <mnpache@gmail.com>
2020-04-08 22:09:34 -0400

    update

--- terraform-oc/provider.tf
+++ terraform-oc/provider.tf
@@ -2,7 +2,7 @@
 provider "kubernetes" {
   host = "https://k-openshift.osh.massopen.cloud:8443"
 
-  token = "31N4zhBNoKz_ymw5zrXZvkioHogv-hWyczqhM-b-4xY"
+  token = "..."
 
   load_config_file = false
 }
